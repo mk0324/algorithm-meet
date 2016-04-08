0a0cfaf2e3bd705ab75b3a3277f31da16c763831
yunhwan1903 <yunhwan1903@gmail.com>
2016-04-08 23:17:52 +0900

    Create yunhwan.java

---
+++ KOOGLE/yunhwan.java
@@ -0,0 +1,51 @@
+package test;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class koogle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		//케이스 
+		int numberOfCase = sc.nextInt(); 
+		for (int i =0; i< numberOfCase; i++){ 
+			
+			double power =0;
+			String code ="";
+	    	
+			//암호 후보
+	    	int numberOfCode = sc.nextInt();     	
+	    	for ( int j=0; j< numberOfCode; j++){ 
+	    		
+	    		int num1 = 0;
+	    		int num2 = 0;
+	    		//강도 구하기
+	    		String cur_code = sc.next();		
+	    		
+	    		for ( int k=0; k<cur_code.length(); k++ ){
+	    			if ( Character.isLowerCase( cur_code.charAt(k) )){
+	    				num1++;}
+	    			else if ( Character.isDigit(cur_code.charAt(k) )){
+	    				num2++;}
+	    		}
+	    		double cur_power = Math.log10( Math.pow(26, num1)*Math.pow(10, num2));	    			    		
+	    		
+	    		//기존 강도와 비교 
+	    		if ( power < cur_power ){
+	    			power = cur_power;
+	    			code = cur_code;
+	    		} 
+	    		else if (power == cur_power) {
+	    			if (code.compareTo(cur_code)>0){
+	    				code = cur_code;}		            
+	    		}	    		
+	    	}
+	    	System.out.println(code);
+	    }
+	 sc.close();
+	}
+}
