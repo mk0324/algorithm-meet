603bd0d730d2f34a4c831ac2354751f2cef8ed86
JANG SUN HYUK <wkdtjsgur100@nate.com>
2016-07-01 01:48:34 +0900

    Update 장선혁.cpp

--- 017_CLIMBINGSTAIR/장선혁.cpp
+++ 017_CLIMBINGSTAIR/장선혁.cpp
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<algorithm>
 
-int stair[301] = { 0 };
-int cache[301] = { 0 };
-int N;
-
 int main()
 {
+	int stair[301] = { 0 };
+	int cache[301] = { 0 };
+	int N;
+
 	scanf("%d", &N);
-	
-	stair[0] = 0;
+
 	for (int i = 1; i <= N;i++)
 		scanf("%d", &stair[i]);
 	
+	cache[0] = 0;
 	cache[1] = stair[1];
 	cache[2] = cache[1] + stair[2];
 	
